0880dd6f837c0ee951fe4f450e16733d8f1bfdb9
Ciaran Bruen <303601+cbruen1@users.noreply.github.com>
2021-03-20 12:59:08 +0000

    Update Controllers, add Swggr filters & Constants.cs

---
+++ AccountAdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerFilterTest.Controllers.v2
+{
+	[Route("api/v{version:apiVersion}/account-admin")]
+	[ApiController]
+	[ApiVersion("2.0")]
+	public class AccountAdminController : ControllerBase
+	{
+		[HttpPost("verify")]
+		[SwaggerOperation(Tags = new[] { Constants.ApiConsumerTagNameConBAccountAdmin, Constants.ApiConsumerTagNameConCAccountAdmin })]
+		public ActionResult Verify([FromBody] string userId)
+		{
+			return Ok($"{userId} V2");
+		}
+	}
+}
---
+++ AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Constants = SwaggerFilterTest.Constants;
+
+namespace SwaggerFilterTest.Controllers.v2
+{
+	[Route("api/v{version:apiVersion}/account")]
+	[ApiController]
+	[ApiVersion("2.0")]
+	public class AccountController : ControllerBase
+	{
+		[HttpGet("get-user-details")]
+		[SwaggerOperation(Tags = new[] { Constants.ApiConsumerTagNameConAAccount })]
+		public ActionResult GetUserDetails([FromQuery]string userId)
+		{
+			return Ok($"{userId} V2");
+		}
+	}
+}
